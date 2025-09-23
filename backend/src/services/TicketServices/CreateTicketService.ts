24c691ad573eada7da4302cdb14d8e146e116bfe
Felipe Perin <felipe_grilanda@hotmail.com>
2025-09-22 22:22:58 -0300

    Correção

--- backend/src/services/TicketServices/CreateTicketService.ts
+++ backend/src/services/TicketServices/CreateTicketService.ts
@@ -21,7 +21,7 @@ const CreateTicketService = async ({
   queueId,
   companyId
 }: Request): Promise<Ticket> => {
-  const defaultWhatsapp = await GetDefaultWhatsApp(companyId);
+  const defaultWhatsapp = await GetDefaultWhatsApp(companyId, queueId);
 
   let ticket = await CheckContactOpenTickets(
     contactId,
